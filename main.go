ffbd023f20ad00b423df70e666e21c301c674b2f
haitammabrouk <haitammk0708@gmail.com>
2026-06-28 10:56:30 +0100

    refactoring...

--- main.go
+++ main.go
@@ -12,37 +12,35 @@ import (
 func main() {
     switch os.Args[1] {
     case "run":
-        run()
+        if err := run(); err != nil {
+			fmt.Fprintf(os.Stderr, "setup namespaces failed: %v\n", err)
+			os.Exit(1)
+		}
     case "child":
-        child()
+        if err := child(); err != nil {
+			fmt.Fprintf(os.Stderr, "init container failed: %v\n", err)
+			os.Exit(1)
+		}
     }
 }
 
-func child() {
-
+func child() error {
 	// swap root fs
-	pivotRoot()
+	if err := setUpRootFs(); err != nil {
+		return err
+	}
 	
 	cmd := exec.Command("/bin/ash")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	
-	if err := cmd.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting the bash process: %v\n", err)
-		os.Exit(1)
-	}
-	
-	if err := cmd.Wait(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error waiting for the bash process: %v\n", err)
-		os.Exit(1)
-	}
+	return cmd.Run()
 }
 
-func run() {
-	// rerun the main program with a child argument
+func run() error {
 	cmd := exec.Command("/proc/self/exe", "child")
-	// set up namespaces (PID, mount, and user)
+
 	cmd.SysProcAttr = &unix.SysProcAttr{
 		Cloneflags: unix.CLONE_NEWUSER | unix.CLONE_NEWPID | unix.CLONE_NEWNS,
 
@@ -52,70 +50,96 @@ func run() {
 		GidMappings: []syscall.SysProcIDMap{
 			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
 		},
-
-		GidMappingsEnableSetgroups: false,
 	}
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error running the child process: %v\n", err)
-		os.Exit(1)
-	}
+	return cmd.Run()
 }
 
-func pivotRoot() error {
-	newRootFsPath, _ := filepath.Abs("./rootfs")
-
-	// make mount points private so that they don't propagate
+func makeMountsPrivate() error {
 	if err := unix.Mount("", "/", "", unix.MS_PRIVATE | unix.MS_REC, ""); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting mount propagation to private: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("make mounts private: %w", err)
+	}
+	return nil
+}
+
+func detachOldRootFs() error {
+	if err := unix.Unmount("./old_root", unix.MNT_DETACH); err != nil {
+		return fmt.Errorf("umount old rootfs: %w", err)
 	}
 
-	// making the new root fs mountable
+	if err := os.RemoveAll("./old_root"); err != nil {
+		return fmt.Errorf("remove old rootfs: %w", err)
+	}
+	return nil
+}
+
+func mountProcFs() error {
+	if err := unix.Mount("proc", "/proc", "proc", 0, ""); err != nil {
+		return fmt.Errorf("mount procfs: %w", err)
+	}
+	return nil
+}
+
+func bindNewMountRootFs(newRootFsPath string) error {
 	if err := unix.Mount(newRootFsPath, newRootFsPath, "", unix.MS_BIND | unix.MS_REC, ""); err != nil {
-		fmt.Fprintf(os.Stderr, "Error making new rootfs mountable: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("bind new rootfs: %w", err)
+	}
+	return nil
+}
+
+func createOldRootFsPlaceholder(oldRootFsPath string) error {
+	if err := os.MkdirAll(oldRootFsPath, 0700); err != nil {
+		return fmt.Errorf("create old rootfs placeholder: %w", err)
+	}
+	return nil
+}
+
+func setWorkingDirToRoot() error {
+	if err := unix.Chdir("/"); err != nil {
+		return fmt.Errorf("change working directory to root: %w", err)
+	}
+	return nil
+}
+
+func setUpRootFs() error {
+	newRootFsPath, err := filepath.Abs("./rootfs")
+
+	if err != nil {
+		return err
 	}
 
 	oldRootFsPath := filepath.Join(newRootFsPath, "old_root")
-	// create the folder inside the new rootfs that will hold old rootfs
-	if err := os.MkdirAll(oldRootFsPath, 0700); err != nil {
-		fmt.Fprintf(os.Stderr, "Errot creating old root folder inside new rootfs: %v\n", err)
-		os.Exit(1)
+
+	if err := makeMountsPrivate(); err != nil {
+		return err
 	}
 
-	// swap the old rootfs with the new rootfs
+	if err := bindNewMountRootFs(newRootFsPath); err != nil {
+		return err
+	}
+
+	if err := createOldRootFsPlaceholder(oldRootFsPath); err != nil {
+		return err
+	}
+
 	if err := unix.PivotRoot(newRootFsPath, oldRootFsPath); err != nil {
-		fmt.Fprintf(os.Stderr, "Error pivoting rootfs: %v\n", err)
-		os.Exit(1)
+		return err
 	}
 
-	// ensure cwd set to new root
-	if err := unix.Chdir("/"); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting the cwd to root: %v\n", err)
-		os.Exit(1)
+	if err := setWorkingDirToRoot(); err != nil {
+		return err
 	}
 
-	// mount the proc and associate it with the pid table of the new PID namespace
-	if err := unix.Mount("proc", "/proc", "proc", 0, ""); err != nil {
-		fmt.Fprintf(os.Stderr, "Error mounting proc: %v\n", err)
-		os.Exit(1)
+	if err := mountProcFs(); err != nil {
+		return err
 	}
 
-	// umount old rootfs
-	if err := unix.Unmount("./old_root", unix.MNT_DETACH); err != nil {
-		fmt.Fprintf(os.Stderr, "Error umounting old rootfs: %v\n", err)
-		os.Exit(1)
-	}
-
-	// remove old rootfs
-	if err := os.RemoveAll("./old_root"); err != nil {
-		fmt.Fprintf(os.Stderr, "Error removing old rootfs: %v\n", err)
-		os.Exit(1)
+	if err := detachOldRootFs(); err != nil {
+		return err
 	}
 
 	return nil
