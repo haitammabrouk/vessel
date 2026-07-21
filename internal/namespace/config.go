f670abf1c88173e3c49a503f6cdea911976b1f5b
haitammabrouk <haitammk0708@gmail.com>
2026-07-21 01:29:31 +0100

    add IPC namespace to isolate access to SysV IPC objects and Posix mqueues

--- internal/namespace/config.go
+++ internal/namespace/config.go
@@ -5,7 +5,8 @@ import (
 )
 
 const cloneFlags = unix.CLONE_NEWPID | 
-				unix.CLONE_NEWNS | unix.CLONE_NEWUTS
+				unix.CLONE_NEWNS | unix.CLONE_NEWUTS |
+				unix.CLONE_NEWIPC
 
 func SetUpNs() *unix.SysProcAttr {
 	return &unix.SysProcAttr{
